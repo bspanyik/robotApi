e3d0f1eb039663f84e9ed6678081d540629dc0f3
Tim Vass <merle.vivo@gmail.com>
2018-04-18 16:34:02 +0200

    RobotPresenter: object model

---
+++ RobotPresenter.php
@@ -0,0 +1,16 @@
+class RobotPresenter
+{	
+
+  protected $id;
+  protected $operator;
+  protected $salary;
+  protected $escaped;
+  protected $room;
+
+  public function __construct()
+  { 	
+    $this->id = $id;
+    $this->operator = $operator;
+    $this->room = $room;
+  }
+}
